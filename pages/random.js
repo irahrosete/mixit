ede2bd92fcc798e7c3f4eea09f50c52d92bb3891
irah rosete <irah.rosete@outlook.com>
2022-03-20 23:25:59 +1100

    update static rendering

--- pages/random.js
+++ pages/random.js
@@ -2,46 +2,58 @@ import { apiRandomDrink } from '../util/apis'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Random.module.css'
+import { useState, useEffect } from 'react'
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
     const res = await fetch(apiRandomDrink)
     const data = await res.json()
     return {
-        props: { randomCocktail: data.drinks[0] }
+        props: { randomDrink: data.drinks[0] }
     }
 }
 
-const Random = ({ randomCocktail }) => {
+const Random = ({ randomDrink }) => {
+    // const [randomDrink, setRandomDrink] = useState()
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const res = await fetch(apiRandomDrink)
+    //         const data = await res.json()
+    //         console.log(data.drinks[0]);
+    //         setRandomDrink(data.drinks[0])
+    //     }
+    //     fetchData()
+    // }, [])
+
     return (<>
         <h1 className='title'>Enjoy your drink!</h1>
         <div className={styles.single}>
-            {console.log(randomCocktail)}
-            <h2 className='title'>{randomCocktail.strDrink}</h2>
+            {/* {console.log(data.drinks[0])} */}
+            <h2 className='title'>{randomDrink.strDrink}</h2>
             <div className={styles.center}>
-            <Image className={styles.image} src={randomCocktail.strDrinkThumb} alt='cocktail' width={500} height={500} />
+                <Image className={styles.image} src={randomDrink.strDrinkThumb} alt='cocktail' width={500} height={500} />
             </div>
             <h4>Ingredients</h4>
-            <p className='text'>{randomCocktail.strMeasure1} {randomCocktail.strIngredient1}</p>
-            <p className='text'>{randomCocktail.strMeasure2} {randomCocktail.strIngredient2}</p>
-            <p className='text'>{randomCocktail.strMeasure3} {randomCocktail.strIngredient3}</p>
-            <p className='text'>{randomCocktail.strMeasure4} {randomCocktail.strIngredient4}</p>
-            <p className='text'>{randomCocktail.strMeasure5} {randomCocktail.strIngredient5}</p>
-            <p className='text'>{randomCocktail.strMeasure6} {randomCocktail.strIngredient6}</p>
-            <p className='text'>{randomCocktail.strMeasure7} {randomCocktail.strIngredient7}</p>
-            <p className='text'>{randomCocktail.strMeasure8} {randomCocktail.strIngredient8}</p>
-            <p className='text'>{randomCocktail.strMeasure9} {randomCocktail.strIngredient9}</p>
-            <p className='text'>{randomCocktail.strMeasure10} {randomCocktail.strIngredient10}</p>
-            <p className='text'>{randomCocktail.strMeasure11} {randomCocktail.strIngredient11}</p>
-            <p className='text'>{randomCocktail.strMeasure12} {randomCocktail.strIngredient12}</p>
-            <p className='text'>{randomCocktail.strMeasure13} {randomCocktail.strIngredient13}</p>
-            <p className='text'>{randomCocktail.strMeasure14} {randomCocktail.strIngredient14}</p>
-            <p className='text'>{randomCocktail.strMeasure15} {randomCocktail.strIngredient15}</p>
+            <p className='text'>{randomDrink.strMeasure1} {randomDrink.strIngredient1}</p>
+            <p className='text'>{randomDrink.strMeasure2} {randomDrink.strIngredient2}</p>
+            <p className='text'>{randomDrink.strMeasure3} {randomDrink.strIngredient3}</p>
+            <p className='text'>{randomDrink.strMeasure4} {randomDrink.strIngredient4}</p>
+            <p className='text'>{randomDrink.strMeasure5} {randomDrink.strIngredient5}</p>
+            <p className='text'>{randomDrink.strMeasure6} {randomDrink.strIngredient6}</p>
+            <p className='text'>{randomDrink.strMeasure7} {randomDrink.strIngredient7}</p>
+            <p className='text'>{randomDrink.strMeasure8} {randomDrink.strIngredient8}</p>
+            <p className='text'>{randomDrink.strMeasure9} {randomDrink.strIngredient9}</p>
+            <p className='text'>{randomDrink.strMeasure10} {randomDrink.strIngredient10}</p>
+            <p className='text'>{randomDrink.strMeasure11} {randomDrink.strIngredient11}</p>
+            <p className='text'>{randomDrink.strMeasure12} {randomDrink.strIngredient12}</p>
+            <p className='text'>{randomDrink.strMeasure13} {randomDrink.strIngredient13}</p>
+            <p className='text'>{randomDrink.strMeasure14} {randomDrink.strIngredient14}</p>
+            <p className='text'>{randomDrink.strMeasure15} {randomDrink.strIngredient15}</p>
             <h4>Instructions</h4>
-            <p className='text'>{randomCocktail.strInstructions}</p>
+            <p className='text'>{randomDrink.strInstructions}</p>
         </div>
-            <Link href='/random'>
-                <a className='btn'>Random Drink</a>
-            </Link>
+        <Link href='/random'>
+            <a className='btn'>Random Drink</a>
+        </Link>
     </>
     )
 }
